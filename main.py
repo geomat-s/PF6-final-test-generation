71a7d6c7a23aabb1d5b99fff0857158c4dd733ba
santi-eg <santiago.espitia@genstudents.org>
2025-12-09 13:03:56 -0500

    Archivo main.py modificado para cumplir con el test y se añadió un print para mostrar información al usuario

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def main():
   user_number = input("Hola, digita un número y te daré información sobre un plato típico de mi país, Colombia: ")
    
   info_dish = dish_fetch(user_number)
-  print(info_dish)
+  #print(info_dish)
+  print(f"El número: {user_number} es el plato llamado: {info_dish["name"]}. \n{info_dish["description"]} Finalmente sus ingredientes son: {info_dish["ingredients"]}")
 
 def dish_fetch(num):
 
