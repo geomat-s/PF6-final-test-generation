e31a50461a34f8a7ba84f251e8ae5c381c233d31
santi-eg <santiago.espitia@genstudents.org>
2025-12-09 12:50:44 -0500

    Archivo main.py modificado con el código para cumplir el reto

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import requests
+import json
+
 def main():
-  print("Hello learners!")
+  user_number = input("Hola, digita un número y te daré información sobre un plato típico de mi país, Colombia: ")
+   
+  info_dish = dish_fetch(user_number)
+  print(info_dish)
+
+def dish_fetch(num):
+
+  response = requests.get(f"https://api-colombia.com/api/v1/TypicalDish/{num}")
+  responseJson = response.json()
+  return responseJson
 
 if __name__=="__main__":
   main()
